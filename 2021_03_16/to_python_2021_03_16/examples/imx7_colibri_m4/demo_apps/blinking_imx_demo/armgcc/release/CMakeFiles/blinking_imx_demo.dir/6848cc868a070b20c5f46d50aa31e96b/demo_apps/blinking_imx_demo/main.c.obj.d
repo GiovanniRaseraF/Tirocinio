3c5149a3b4447a7330ea995e5f27bf2efcfc841f
GiovanniRaseraF <fdsa@DESKTOP-KFD3S9A.localdomain>
2021-03-16 14:24:34 +0100

    fdsa

---
+++ 2021_03_16/to_python_2021_03_16/examples/imx7_colibri_m4/demo_apps/blinking_imx_demo/armgcc/release/CMakeFiles/blinking_imx_demo.dir/6848cc868a070b20c5f46d50aa31e96b/demo_apps/blinking_imx_demo/main.c.obj.d
@@ -0,0 +1,102 @@
+CMakeFiles/blinking_imx_demo.dir/6848cc868a070b20c5f46d50aa31e96b/demo_apps/blinking_imx_demo/main.c.obj: \
+ C\:\Users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\demo_apps\blinking_imx_demo\main.c \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\board.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\pin_mux.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\device_imx.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\MCIMX7D/include/MCIMX7D_M4.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cm4.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cminstr.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cmfunc.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cmsimd.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\mcimx7d\startup\system_mcimx7d_m4.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\rdc.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\rdc_defs_imx7d.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\ccm_imx7d.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\clock_freq.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\gpio_pins.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\gpio_imx.h \
+ C\:\Users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\demo_apps\blinking_imx_demo\gpio_ctrl.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\utilities\inc\debug_console_imx.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\rpmsg\rpmsg_rtos.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\rpmsg\rpmsg.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\rpmsg\rpmsg_core.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\env\env.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\env\freertos\rpmsg_porting.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\imx7d_m4\rpmsg_platform_porting.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\virtio\virtio.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\virtio\virtqueue.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\virtio\virtio_ring.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\env\env.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\common\llist\llist.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\common\hil\hil.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\virtio\virtio.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\config\config.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\env\env.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\common\shm\sh_mem.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\env\env.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\common\llist\llist.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\rpmsg\rpmsg.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\freertos.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\demo_apps\blinking_imx_demo\freertosconfig.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\projdefs.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\portable.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\deprecated_definitions.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\portable\gcc\arm_cm4f\portmacro.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\mpu_wrappers.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\task.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\list.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\gpt.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\semphr.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\queue.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\mu_imx.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\rdc_semaphore.h
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\board.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\pin_mux.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\device_imx.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\MCIMX7D/include/MCIMX7D_M4.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cm4.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cminstr.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cmfunc.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cmsimd.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\mcimx7d\startup\system_mcimx7d_m4.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\rdc.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\rdc_defs_imx7d.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\ccm_imx7d.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\clock_freq.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\gpio_pins.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\gpio_imx.h:
+C\:\Users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\demo_apps\blinking_imx_demo\gpio_ctrl.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\utilities\inc\debug_console_imx.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\rpmsg\rpmsg_rtos.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\rpmsg\rpmsg.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\rpmsg\rpmsg_core.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\env\env.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\env\freertos\rpmsg_porting.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\imx7d_m4\rpmsg_platform_porting.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\virtio\virtio.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\virtio\virtqueue.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\virtio\virtio_ring.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\env\env.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\common\llist\llist.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\common\hil\hil.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\virtio\virtio.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\config\config.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\env\env.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\common\shm\sh_mem.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\porting\env\env.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\common\llist\llist.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\middleware\multicore\open-amp\rpmsg\rpmsg.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\freertos.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\demo_apps\blinking_imx_demo\freertosconfig.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\projdefs.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\portable.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\deprecated_definitions.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\portable\gcc\arm_cm4f\portmacro.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\mpu_wrappers.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\task.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\list.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\gpt.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\semphr.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\rtos\freertos\source\include\queue.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\mu_imx.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\drivers\inc\rdc_semaphore.h:
---
+++ 2021_03_16/to_python_2021_03_16/examples/imx7_colibri_m4/demo_apps/blinking_imx_demo/armgcc/release/CMakeFiles/blinking_imx_demo.dir/8c0c73dda25281aef161896a1e605937/examples/imx7_colibri_m4/pin_mux.c.obj.d
@@ -0,0 +1,18 @@
+CMakeFiles/blinking_imx_demo.dir/8c0c73dda25281aef161896a1e605937/examples/imx7_colibri_m4/pin_mux.c.obj: \
+ C\:\Users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\pin_mux.c \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\device_imx.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\MCIMX7D/include/MCIMX7D_M4.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cm4.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cminstr.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cmfunc.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cmsimd.h \
+ c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\mcimx7d\startup\system_mcimx7d_m4.h \
+ C\:\Users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\pin_mux.h
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\device_imx.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\MCIMX7D/include/MCIMX7D_M4.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cm4.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cminstr.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cmfunc.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\cmsis\include\core_cmsimd.h:
+c\:\users\giovannirasera\eclipse-workspace\to_python_2021_03_16\platform\devices\mcimx7d\startup\system_mcimx7d_m4.h:
+C\:\Users\giovannirasera\eclipse-workspace\to_python_2021_03_16\examples\imx7_colibri_m4\pin_mux.h:
