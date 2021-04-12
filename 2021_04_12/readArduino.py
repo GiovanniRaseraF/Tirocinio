ad871b99323f702744f6697d1f62b4a5cd8338aa
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2021-04-12 17:32:56 +0200

    fdjskla

--- 2021_04_12/readArduino.py
+++ 2021_04_12/readArduino.py
@@ -2,13 +2,35 @@
 import serial
 import syslog
 import time
+import os
+import datetime
+import signal
+import sys
+fileLog = None
+
+#Listen to Ctrl+C
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    #Close file
+    fileLog.close()
+    sys.exit(0)
 
 #The following line is for serial over GPIO
 port = '/dev/ttyACM0'
 
-
 ard = serial.Serial(port, 9600)
 
-while(True):
-    msg = ard.readline()
-    print(msg)
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    #Logging file
+    fileLog = open("fileLog.csv","a")
+
+    while(True):
+        line = (ard.readline()).decode('UTF-8')
+         #Estract delta time
+        co2 = line.split(',')
+        current_time = datetime.datetime.now()
+        #Write to file
+        fileLog.write("%s,%s\n" % (current_time, co2))
+        fileLog.flush()
+        print(line)
